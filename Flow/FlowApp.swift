4b8ef8843dda982f32096c5a8cdba95444abb704
Antocraft83100 <luge-03.dispute@icloud.com>
2025-11-18 23:56:36 +0100

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Antoine BLEUZE on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
